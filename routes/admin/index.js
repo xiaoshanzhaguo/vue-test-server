9d65454e5746230ecdf44e76a1e75114bf6b566e
shanzhaguo <xiaoshanzhaguo@163.com>
2022-03-12 15:04:16 +0800

    complete 用户搜索

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -6,20 +6,20 @@ module.exports = app => {
     // 1.3 这里是express的一个子路由，当我们需要子路由的时候，就用它。
     const router = express.Router()
     // 1.6
-    const User = require('../../models/User')  // !!!
+    const User = require('../../models/User') // !!!
     // 1.5 这里我们会涉及到把数据存进去，因此需要数据库
 
     // 添加用户
     router.post('/users', async (req, res) => {
         // 1.7 引入User后，继续把数据存进去（下面要想使用的话，需要添加await）
-        const model = await User.create(req.body)// 定义一个model
+        const model = await User.create(req.body) // 定义一个model
         // 1.8 发回客户端，让它知道我们创建完成了，以及创建的数据是什么
         res.send(model)
     })
-    
+
     // 获取用户列表
     router.get('/users', async (req, res) => {
-        const items = await User.find().limit(10)  // !!!
+        const items = await User.find().limit(10) // !!!
         res.send(items)
     })
 
@@ -28,7 +28,7 @@ module.exports = app => {
         const model = await User.findById(req.params.id)
         res.send(model)
     })
-    
+
     // 编辑用户
     router.put('/users/:id', async (req, res) => {
         const model = await User.findByIdAndUpdate(req.params.id, req.body)
@@ -41,6 +41,17 @@ module.exports = app => {
         res.send({
             success: true
         })
+    })
+
+    // 查找用户
+    router.get('/user/:username', async (req, res) => {
+        var username = req.params.username;
+        // 正则匹配 i忽略大小写
+        var reg = new RegExp(username, "i")
+        const model = await User.find({
+            username: {$regex: reg}
+        })
+        res.send(model)
     })
     // 1.4 将子路由挂载到这个地方，不然每次写路由都要加上/admin/api很麻烦
     app.use('/admin/api', router)
