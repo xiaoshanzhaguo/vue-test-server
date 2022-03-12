af8fb860fe038735ae4dafe7ee87a07b755c7e0f
shanzhaguo <xiaoshanzhaguo@163.com>
2022-03-12 16:26:07 +0800

    complete table列表里密码显示与隐藏

--- models/User.js
+++ models/User.js
@@ -1,10 +1,21 @@
-
 const moogose = require('mongoose')
 
 // 导出再建立一个schema,用它去定义模型的字段有哪些
 const schema = new moogose.Schema({
-    username: { type: String },
-    password: { type: String }
+    username: {
+        type: String
+    },
+    password: {
+        type: String
+    },
+    password_d: {
+        type: String,
+        default: "******"
+    },
+    icon: {
+        type: String,
+        default: "iconfont icon-eye-none"
+    }
 })
 
 // 导出mongoose.model (弹幕：实例这个user并导出)  ！！！！
